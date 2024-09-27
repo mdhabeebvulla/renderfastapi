f8d94a1435901165502e1f45011a87eef0225dbf
mdhabeebvulla <55741151+mdhabeebvulla@users.noreply.github.com>
2024-09-27 23:07:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+iris = load_iris()
+X = iris.data
+y = iris.target
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+lr.fit(X_train,y_train)
 app = FastAPI()
 @app.get("/")
-def example():
-    return{'message':'welcome to render deployment'}
+def example(sl,sw,pl,pw):
+    result = lr.predict([[sl,sw,pl,pw]])
+    return{'message':int(result[0])}
